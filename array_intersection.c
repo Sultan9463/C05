7ea082cff89e0d71f74ccaa8d766eca3b83d11c4
Sultan9463 <s75696643@gmail.com>
2025-07-04 12:54:32 +0100

    Update array_intersection.c

--- array_intersection.c
+++ array_intersection.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
 
+#define MAX_SIZE 100
+
 int main() {
-    int n1, n2;
-    // Lire la taille du premier tableau
+    int n1, n2, arr1[MAX_SIZE], arr2[MAX_SIZE], inter[MAX_SIZE];
+    int i, j, k = 0, found;
+
+    // Lire la taille et les éléments du premier tableau
     scanf("%d", &n1);
-    //lire les elements du premier tableau
-    int tab1[n1];
-    for (int i = 0; i < n1; i++) {
-        scanf("%d", &tab1[i]);
-    }
-    // Lire la taille du deuxieme tableau
+    for(i = 0; i < n1; i++)
+        scanf("%d", &arr1[i]);
+
+    // Lire la taille et les éléments du deuxième tableau
     scanf("%d", &n2);
-    int tab2[n2];
-    //lire les elements du deuxieme tableau
-    for (int i = 0; i < n2; i++) {
-        scanf("%d", &tab2[i]);
-    }
-        // Afficher les elements communs
-        for (int i = 0; i < n1; i++) {
-            int courant = tab1[i];
-            int deja_afficher = 0;
-            // verifie si deja_afficher 
-                for (int k = 0; k < i; k++) {
-                    if (tab1[k] == courant) {
-                    deja_afficher = 1;
-                    break;
+    for(i = 0; i < n2; i++)
+        scanf("%d", &arr2[i]);
+
+    // Trouver l'intersection sans doublons
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(arr1[i] == arr2[j]) {
+                // Vérifier si déjà ajouté
+                found = 0;
+                for(int l = 0; l < k; l++) {
+                    if(inter[l] == arr1[i]) {
+                        found = 1;
+                        break;
+                    }
                 }
+                if(!found) {
+                    inter[k++] = arr1[i];
+                }
+                break;
             }
-            if (deja_afficher) continue;
-            // chercher si courant existe dant tab2 
-            for (int j = 0; j < n2; j++) {
-                if (courant == tab2[j]) {
-                    printf("%d", courant);
-                    break;
-                }
-            }   
         }
-        printf("\n");
-        return 0;
-    }   
+    }
+
+    // Afficher le résultat
+    printf("Intersection :");
+    for(i = 0; i < k; i++)
+        printf(" %d", inter[i]);
+    printf("\n");
+
+    return 0;
+}
